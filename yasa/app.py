2ae7f1e0aa09db579c0f4b9d1dbf2255ca3fa76d
Joker Qyou <Joker.Qyou@gmail.com>
2016-01-10 00:00:32 +0800

    A useful monitor app now

--- yasa/app.py
+++ yasa/app.py
@@ -1,14 +1,25 @@
 # coding: utf-8
-import netifaces
+from __future__ import unicode_literals
+# import netifaces
+import requests
 import rumps
+import webbrowser
 
 
 class Yasa(rumps.App):
 
-    @rumps.clicked('Hi')
-    def sayhi(self, _):
-        rumps.notification('Hi', 'sub title', 'Hello rumps!')
+    @rumps.clicked('实时图像（静态）')
+    def sayhi(self, sender):
+        webbrowser.open('http://192.168.1.233:9876/camera/shot')
+
+    @rumps.timer(60)
+    def show_temperature(self, sender):
+        data = requests.get(
+            'http://192.168.1.233:9876/sensors/temperature'
+        ).json()
+        self.title = '气温 {:.2f}°C'.format(
+            data['temperature']
+        )
 
 if __name__ == '__main__':
     Yasa('Yasa').run()
-
